1b26b44249d634930e4235d3a06a094b8e5d04fe
sawal <sawalmaskey@hotmail.com>
2018-08-05 14:04:58 -0500

    Added a new planet and fixed typos in game description

--- sql/data1.sql
+++ sql/data1.sql
@@ -1,8 +1,8 @@
 INSERT INTO planet (planet_name, planet_image, difficulty_level, initial_energy) 
-VALUES ('Vulcan', 'vulcan.jpg', 6, 220);
+VALUES ('Andoria', 'andoria.jpg', 7, 200);
 
-INSERT INTO item (item_name, item_image) 
-VALUES ('Ozone', 'ozone.jpg');
+-- INSERT INTO item (item_name, item_image) 
+-- VALUES ('Ozone', 'ozone.jpg');
 
 
 
@@ -10,38 +10,26 @@ VALUES ('Ozone', 'ozone.jpg');
 INSERT INTO planet_item_goal (planet_id, item_id, required_qty)
 SELECT p.planet_id, i.item_id, 60
 FROM planet p, item i
-WHERE p.planet_name = 'Vulcan' AND i.item_name = 'Carbon Dioxide'
+WHERE p.planet_name = 'Andoria' AND i.item_name = 'Oxygen'
 UNION 
-SELECT p.planet_id, i.item_id, 40
+SELECT p.planet_id, i.item_id, 120
 FROM planet p, item i
-WHERE p.planet_name = 'Vulcan' AND i.item_name = 'Oxygen'
-UNION 
-SELECT p.planet_id, i.item_id, 100
-FROM planet p, item i
-WHERE p.planet_name = 'Vulcan' AND i.item_name = 'Water';
+WHERE p.planet_name = 'Andoria' AND i.item_name = 'Water';
 
 
 INSERT INTO planet_item_init_resource (planet_id, item_id, available_qty) 
-SELECT p.planet_id, i.item_id, 90
+SELECT p.planet_id, i.item_id, 60
 FROM planet p, item i
-WHERE p.planet_name = 'Vulcan' AND i.item_name = 'Sulphur'
+WHERE p.planet_name = 'Andoria' AND i.item_name = 'Methane'
 UNION 
 SELECT p.planet_id, i.item_id, 60
 FROM planet p, item i
-WHERE p.planet_name = 'Vulcan' AND i.item_name = 'Carbon'
-UNION
-SELECT p.planet_id, i.item_id, 100
-FROM planet p, item i
-WHERE p.planet_name = 'Vulcan' AND i.item_name = 'Ozone'
-UNION 
-SELECT p.planet_id, i.item_id, 200
-FROM planet p, item i
-WHERE p.planet_name = 'Vulcan' AND i.item_name = 'Hydrogen';
+WHERE p.planet_name = 'Andoria' AND i.item_name = 'Ozone';
 
 INSERT INTO robot_type (robot_type_name, time_required, robot_type_image, initial_energy_cost)
-VALUES ('Carbon Ozonizer', 90, 'vinci.jpg', 50), ('Sulphur Ozonizer', 120, 'fi.jpg', 100);
+VALUES ('Carbon Generator', 30, 'fig.jpg', 50);
 
-
+/*
 INSERT INTO combiner (combiner_id, produce_item_id, energy_required, qty_produced)
 SELECT rt.robot_type_id, i.item_id, 10, 3
 FROM robot_type rt, item i
@@ -75,40 +63,25 @@ FROM robot_type rt, (
     WHERE item_name = 'Ozone'
 ) i
 WHERE rt.robot_type_name = 'Sulphur Ozonizer';
+*/
 
 INSERT INTO Diffusor (diffusor_id, consume_item_id, energy_released, energy_limit)
-SELECT rt.robot_type_id, i.item_id, 20, 500
+SELECT rt.robot_type_id, i.item_id, 2, 200
 FROM robot_type rt, item i
-WHERE rt.robot_type_name = 'Sulphur Generator' AND i.item_name = 'Sulphur Dioxide'
-UNION
-SELECT rt.robot_type_id, i.item_id, 5, 500
-FROM robot_type rt, item i
-WHERE rt.robot_type_name = 'Methane Combustor' AND i.item_name = 'Methane';
+WHERE rt.robot_type_name = 'Carbon Generator' AND i.item_name = 'Carbon Dioxide';
 
 INSERT INTO produce_diffusor (diffusor_id, produce_item_id, qty_produced)
 SELECT rt.robot_type_id, i.item_id, i.qty 
 FROM robot_type rt, (
     SELECT item_id, 1 qty
     FROM item
-    WHERE item_name = 'Sulphur'
+    WHERE item_name = 'Carbon'
     UNION
     SELECT item_id, 2 qty
     FROM item
     WHERE item_name = 'Oxygen'
 ) i
-WHERE rt.robot_type_name = 'Sulphur Generator'
-UNION 
-SELECT rt.robot_type_id, i.item_id, i.qty 
-FROM robot_type rt, (
-    SELECT item_id, 1 qty
-    FROM item
-    WHERE item_name = 'Carbon'
-    UNION
-    SELECT item_id, 4 qty
-    FROM item
-    WHERE item_name = 'Hydrogen'
-) i
-WHERE rt.robot_type_name = 'Methane Combustor';
+WHERE rt.robot_type_name = 'Carbon Generator';
 
 
 
